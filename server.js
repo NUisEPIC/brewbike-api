6c19b0f433e91fc6a90996aa6fed3ad70426304b
AnxChow <ankitachowdhry97@gmail.com>
2017-11-09 16:40:08 -0600

    added cors functionality

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ import router from './router';
 const app = express();
 
 var bodyParser = require('body-parser');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,7 +23,7 @@ app.use('/v1', router);
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/shops', function(err){
-  const server = app.listen(3000, () => {
+  const server = app.listen(2000, () => {
     const { address, port } = server.address();
     console.log(`Listening at http://${address}:${port}`);
   });
